63db007e5fc3ae4a15e94971217e23a9c8123759
Luckyreddy08 <160878409+Luckyreddy08@users.noreply.github.com>
2024-04-27 16:02:03 +0530

    Add files via upload

---
+++ Movieimpl.java
@@ -0,0 +1,8 @@
+public class Movieimpl{	
+		public static void main(String[] args){
+			Movie mv=new Movie("Arya 2",2008);
+			mv.display();
+			ActionMovie am=new ActionMovie("Puspha 2",2024,"ALLU ARJUN");
+			am.display();
+		}
+}
